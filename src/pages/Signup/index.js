70f2d81e81d7757910e0df4f88c0e9f403eb7837
AbelGoncalo <abelgoncalo8@gmail.com>
2025-07-16 02:25:16 +0100

    feat: basic signup

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,28 +1,54 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import { useState } from 'react'
 
 import { Box, Field, Button } from '~/components/'
 
-const Main = styled(Box)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  flex-direction: column;
-`
-
 export const Signup = () => {
+  const [values, setValues] = useState({})
+
+  const onChange = ev => {
+    setValues(prev => ({
+      ...prev,
+      [ev.target.name]: ev.target.value,
+    }))
+  }
+
+  const onSubmit = () => {
+    EvalError.preventDefault()
+    console.log(values)
+  }
+
   return (
-    <Main as="main" flexbox="column" center>
+    <Box flex={1} flexbox="column" center>
       <Box style={{ width: 380 }}>
-        <Field type="text" name="name" label="Nome" mb={3} />
-        <Field type="email" name="email" label="E-mail" mb={3} />
-        <Field type="password" name="password" label="Senha" mb={3} />
+        <form onSubmit={onSubmit}>
+          <Field
+            type="text"
+            name="name"
+            label="Nome"
+            mb={3}
+            onChange={onChange}
+          />
+          <Field
+            type="email"
+            name="email"
+            label="E-mail"
+            mb={3}
+            onChange={onChange}
+          />
+          <Field
+            type="password"
+            name="password"
+            label="Senha"
+            mb={3}
+            onChange={onChange}
+          />
+        </form>
       </Box>
 
       <Box flexbox center>
         <Button>Registrar</Button>
       </Box>
-    </Main>
+    </Box>
   )
 }
